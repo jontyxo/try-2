3514e3970fe959a9fa36f1d99733ace578cedda5
Joyesh Wadhwa <joyesh16wa@gmail.com>
2022-11-10 00:47:51 +0530

    first commit

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,49 @@
+import "./register.css"
+import {useRef,useState} from "react"
+import Axios from "axios";
+import axios from "axios";
+
+
+export default function Register() {
+  const usernameRef=useRef();
+  const emailRef=useRef();
+  const passwordRef=useRef();
+  const [err,setErr] =useState(false);
+  const errmessage=()=>{
+setErr(true);
+  }
+
+ const registerHandler=(e)=>{
+  e.preventDefault();
+   axios.post('https://test-test-xenon.herokuapp.com/api/users/register',{
+     name:usernameRef.current.value,
+     email:emailRef.current.value,
+     password:passwordRef.current.value
+   }).then(()=>{
+    window.location.href = '/login'
+   }).catch(errmessage)
+ }
+  
+ const goToLogin=()=>{
+  window.location.href = '/login'
+ }
+
+
+
+    return (
+        <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm" onSubmit={registerHandler}>
+        <label>Username</label>
+        <input className="registerInput" type="text" placeholder="Enter your username..." ref={usernameRef} />
+        <label>Email</label>
+        <input className="registerInput" type="text" placeholder="Enter your email..." ref={emailRef} />
+        <label>Password</label>
+        <input className="registerInput" type="password" placeholder="Enter your password..." ref={passwordRef} />
+        <button className="registerButton">Register</button>
+      </form>
+        <button className="registerLoginButton" onClick={goToLogin}>Login</button>
+        {err && <span>Something went wrong!</span>}
+    </div>
+    )
+}
